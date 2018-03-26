17dc6f64bfcc484fe62ccbda76527593c5500471
ec16439 <s.atemie@se16.qmul.ac.uk>
2018-03-26 16:09:34 +0100

    updated timestamps

--- src/sample/DealAdminController.java
+++ src/sample/DealAdminController.java
@@ -1,10 +1,14 @@
 package sample;
 
+import java.util.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import java.io.*;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class DealAdminController {
     @FXML
@@ -19,7 +23,17 @@ public class DealAdminController {
     TextField txtPromoCode;
 
 public void btnSubmitDeal(ActionEvent actionEvent){
+    LocalDate sDate = StartDate.getValue();
+    Calendar c1 = Calendar.getInstance();
+    c1.set(sDate.getYear(),sDate.getMonthValue()-1,sDate.getDayOfMonth());
+    Date start = c1.getTime();
+    //System.out.println(c1.getTime());
+    LocalDate eDate = EndDate.getValue();
+    Calendar c2 = Calendar.getInstance();
+    //System.out.println(c2.getTime());
+    c2.set(eDate.getYear(),eDate.getMonthValue()-1,eDate.getDayOfMonth());
+    Date end = c2.getTime();
 
-    //Deal nDeal = new Deal(txtDealName.getText(),StartDate.getValue(),EndDate.getValue(),txtPromoCode.getText(),txtSummary.getText());
+    Deal nDeal = new Deal(txtDealName.getText(),start,end,txtPromoCode.getText(),txtSummary.getText());
 }
 }
--- src/sample/DealDisplayController.java
+++ src/sample/DealDisplayController.java
@@ -1,4 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class DealDisplayController {
+    @FXML
+    Button btnReturn;
+    public void returnLogin(ActionEvent actionEvent) throws Exception {
+        //System.out.print("Reached showdeal");
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene nScene = new Scene(root);
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        //primaryStage.hide();
+        primaryStage.setScene(nScene);
+        //primaryStage.show();
+    }
 }
